51f9185d19aaa457ee4a4f5e883162e881996a3a
eliguy343 <eliguy343@gmail.com>
2024-09-23 19:27:26 +0300

    navbar

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,11 +1,41 @@
 "use client";
-import React from 'react'
+import Image from 'next/image';
+import React, { useState } from 'react'
+import BellIcon from '../assets/bell.png';
+import ProfileIcon from '../assets/profile-user.png';
+import CartIcon from '../assets/shopping-cart.png';
+
 
 
 const NavIcons = () => {
-  return (
-    <div>NavIcons</div>
-  )
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+    
+  return <div className='flex items-center gap-4 xl:gap-6'>
+      <Image
+        src={ProfileIcon}
+        alt=""
+        width={22}
+        height={22}
+        className='cursor-pointer'
+      />
+      <Image
+        src={BellIcon}
+        alt=""
+        width={22}
+        height={22}
+        className='cursor-pointer'
+      />
+      <Image
+        src={CartIcon}
+        alt=""
+        width={22}
+        height={22}
+        className='cursor-pointer'
+      />
+
+    </div>
+  
 }
 
 export default NavIcons
