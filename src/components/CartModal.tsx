da0b6417a6ccd7f18046634ff8f62c8b5ec06b5e
eliguy343 <eliguy343@gmail.com>
2024-09-26 23:36:06 +0300

    item list in cart

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -10,7 +10,7 @@ const CartModal = () => {
 
   return (
     <div
-      className='absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]
+      className='w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)]
       bg-white top-12 right-0 flex flex-col gap-6 z-20  '
     >
       {!cartItems ? (
@@ -18,25 +18,54 @@ const CartModal = () => {
           Cart is Empty
         </div>
       ) : (
-        <div>
-          <Image
-            src="https://i.imgur.com/GRz3dtd.png"
-            alt=''
-            width={72}
-            height={92}
-            className='object-cover rounded-md'
-          />
-          <div className=''>
-            <div className=''>
+        <div className='flex flex-col gap-8'>
+          <div className='flex gap-4'>
+            <Image
+              src="https://i.imgur.com/GRz3dtd.png"
+              alt=''
+              width={72}
+              height={92}
+              className='object-cover rounded-md'
+            />
+            <div className='flex flex-col justify-between w-full'>
               <div className=''>
-                <h3>Product Name</h3>
-                <div className=''>$49</div>
+                <div className='flex items-center justify-between gap-8'>
+                  <h3 className='font-semibold'>Product Name</h3>
+                  <div className='p-1 bg-gray-50 rounded-sm'>$49</div>
+                </div>
+                <div className='text-sm text-gray-500'>
+                  available 
+                </div>
               </div>
-              <div className=''>
-                available 
+              <div className='flex justify-between  '>
+                <span className='text-gray-500'>Qty .2</span>
+                <span className='text-blue-500  '>Remove</span>
               </div>
             </div>
-
+          </div>
+          <div className='flex gap-4'>
+            <Image
+              src="https://i.imgur.com/GRz3dtd.png"
+              alt=''
+              width={72}
+              height={92}
+              className='object-cover rounded-md'
+            />
+            <div className='flex flex-col justify-between w-full'>
+              <div className=''>
+                <div className='flex items-center justify-between gap-8'>
+                  <h3 className='font-semibold'>Product Name</h3>
+                  <div className='p-1 bg-gray-50 rounded-sm'>$49</div>
+                </div>
+                <div className='text-sm text-gray-500'>
+                  available 
+                </div>
+              </div>
+              <div className='flex justify-between  '>
+                <span className='text-gray-500'>Qty .2</span>
+                <span className='text-blue-500  '>Remove</span>
+              </div>
+            </div>
           </div>
         </div>
       )}
