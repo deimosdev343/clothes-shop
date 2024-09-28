21c5a4bd8535966e82ca6583f29fc9565db3418a
eliguy343 <eliguy343@gmail.com>
2024-09-28 19:59:37 +0300

    slide controls css

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -41,16 +41,16 @@ const Slider = () => {
     <div className='h-[calc(100vh-80px)] overflow-hidden'>
       <div className='w-max h-full flex transition-all ease-in-out duration-1000'>
         {slides.map(slide => <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
-          <div className='h-1/2 xl:w-1/2'>
-            <h2>{slide.description}</h2>
-            <h1>{slide.title}</h1>
+          <div className='h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center'>
+            <h2 className='text-xl lg:text-3xl 2xl:text-5xl'>{slide.description}</h2>
+            <h1 className='text-5xl lg:text-6xl 2xl:text-8xl font-semibold'>{slide.title}</h1>
             <Link href={slide.url}>
-              <button>
+              <button className='rounded-md bg-black text-white py-3 px-4'>
                 SHOP NOW
               </button>
             </Link>
           </div>
-          <div className=' w-1/2 relative'>
+          <div className='h-1/2 xl: w-1/2 relative xl:h-full'>
             <Image
               src="https://i.imgur.com/1uMluMJ.png"
               alt=''
@@ -60,6 +60,21 @@ const Slider = () => {
             />
           </div>
         </div>)}
+      </div>
+      <div className='absolute m-auto left-1/2 bottom-8 flex gap-4'>
+        {
+          slides.map((slide, index) => (
+            <div 
+              className={`w-3 h-3 rounded-full ring-1 ring-gray-600
+                cursor-pointer flex items-center justify-center ${current === index ? "scale-150" : ""}`}
+            >
+              {current === index && 
+                <div className='w-[6px] h-[6px] bg-gray-600 rounded-full'>
+                </div>
+              }
+            </div>
+          ))
+        }      
       </div>
     </div>
   )
