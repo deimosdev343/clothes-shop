2bcd7494e743c1298f6621fc978ce506f978e713
eliguy343 <eliguy343@gmail.com>
2024-09-28 20:05:36 +0300

    slide functionality

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -39,7 +39,10 @@ const Slider = () => {
   const [current, setCurrent] = useState(0);
   return (
     <div className='h-[calc(100vh-80px)] overflow-hidden'>
-      <div className='w-max h-full flex transition-all ease-in-out duration-1000'>
+      <div 
+        className='w-max h-full flex transition-all ease-in-out duration-1000'
+        style={{transform:`translateX(-${current *100}vw)`}}
+      >
         {slides.map(slide => <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
           <div className='h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center'>
             <h2 className='text-xl lg:text-3xl 2xl:text-5xl'>{slide.description}</h2>
@@ -67,9 +70,12 @@ const Slider = () => {
             <div 
               className={`w-3 h-3 rounded-full ring-1 ring-gray-600
                 cursor-pointer flex items-center justify-center ${current === index ? "scale-150" : ""}`}
+              onClick={() => {
+                setCurrent(index);
+              }}
             >
               {current === index && 
-                <div className='w-[6px] h-[6px] bg-gray-600 rounded-full'>
+                <div   className='w-[6px] h-[6px] bg-gray-600 rounded-full'>
                 </div>
               }
             </div>
