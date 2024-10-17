8df98f4e8a67eb7eb2b6bc5a8f4bfbe67cb76689
eliguy343 <eliguy343@gmail.com>
2024-10-18 00:23:39 +0300

    footer struct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold">New Products</h1>
         <ProductList/>
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <Footer/>
-      </div>
+  
     </div>
 
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,20 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div>
-
-      </div>
-      <div>
-        <div className='w-1/4'>
+      <div className='flex justify-between gap-24'>
+        <div className='w-1/4 flex flex-col gap-8'>
           <Link href='/'>
             <div className='text-2xl font-extrabold'>Cloths Shop</div>
           </Link>
+          
+          <p>
+            3252 Winslow Way NW OR 97304, United States
+          </p>
+          <span>Vasyan@VasyankaMasters.org</span>
+          <span>+972 54 7252158</span>
+          <div className='flex gap-6'>
+
+          </div>
         </div>
         <div className='w-1/2'>
 
