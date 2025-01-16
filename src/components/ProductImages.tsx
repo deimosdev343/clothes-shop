18fa4c780c615a252211acde408e98fa3a801c40
eliguy343 <eliguy343@gmail.com>
2025-01-16 21:50:54 +0200

    product page

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -2,24 +2,6 @@
 
 import Image from "next/image"
 import { useState } from "react"
-const images = [
-  {
-    id:1,
-    url:"https://i.imgur.com/fzh8p.png"
-  },
-  {
-    id:2,
-    url:"http://i.imgur.com/6rJuQ.jpg"
-  },
-  {
-    id:3,
-    url:"http://i.imgur.com/oXxwC.jpg"
-  },
-  {
-    id:4,
-    url:"http://i.imgur.com/y6w0T.jpg"
-  }
-]
 
 const ProductImages = ({pictures}: {pictures: Array<string>})  => {
   const [index, setIndex] = useState(0);
