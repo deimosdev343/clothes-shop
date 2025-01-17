1f339553e8adb16c7a513284188210e1f6e47e6e
eliguy343 <eliguy343@gmail.com>
2025-01-17 17:56:08 +0200

    colors

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -1,25 +1,27 @@
 "use client";
 
+import { useState } from "react";
+
 
 export const CustomizeProducts = ({colors, sizes} : {colors: Array<string>, sizes: Array<string>}) => {
-  console.log(colors);
+  const [colorIndex, setColorIndex] = useState(0);
+  
   return (
     <div className="flex flex-col gap-6">
       <h4 className="font-medium">Choose a color</h4>
       <ul className="flex items-center gap-5">
         {/*This is how we are going to do the color select, not too bad*/}
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 
-          cursor-pointer relative bg-red-500"
+        {colors.map(clr => <li
+          className={`w-8 h-8 rounded-full ring-1 ring-gray-300 
+            cursor-pointer relative`}
+          style={{
+            backgroundColor:clr
+          }} 
         >
           <div className="absolute w-10 h-10 rounded-full ring-2 
             top-[-10%] left-1/2 transform -translate-x-1/2 -tranlsate-y-1/2"/>
-        </li>
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 
-          cursor-pointer relative bg-blue-500"
-        >
-          <div className="absolute w-10 h-10 rounded-full ring-2 top-[-10%] 
-            left-1/2 transform -translate-x-1/2 -tranlsate-y-1/2"/>
-        </li>
+        </li>)}
+       
       </ul>
       <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
