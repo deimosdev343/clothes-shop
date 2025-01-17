f6a6b44df1d4f10ff8f6f3dc1b2556f4266d6c85
eliguy343 <eliguy343@gmail.com>
2025-01-17 23:23:00 +0200

    color and size select

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -4,36 +4,48 @@ import { useState } from "react";
 
 
 export const CustomizeProducts = ({colors, sizes} : {colors: Array<string>, sizes: Array<string>}) => {
-  const [colorIndex, setColorIndex] = useState(0);
+  const [index, setIndex] = useState({
+    colorIndex:0,
+    sizeIndex:0
+  })
   
   return (
     <div className="flex flex-col gap-6">
       <h4 className="font-medium">Choose a color</h4>
       <ul className="flex items-center gap-5">
         {/*This is how we are going to do the color select, not too bad*/}
-        {colors.map(clr => <li
-          className={`w-8 h-8 rounded-full ring-1 ring-gray-300 
+        {colors.map((clr, i) => <li
+          className={`w-8 h-8 rounded-full ring-2 ${i !== index.colorIndex ? "ring-gray-300" : "ring-blue-800"} 
             cursor-pointer relative`}
           style={{
             backgroundColor:clr
-          }} 
+          }}
+          onClick={() => {
+            setIndex(ind => ({...ind, colorIndex: i}))
+          }}
         >
-          <div className="absolute w-10 h-10 rounded-full ring-2 
-            top-[-10%] left-1/2 transform -translate-x-1/2 -tranlsate-y-1/2"/>
+          {i === index.colorIndex && <div className="absolute w-10 h-10 rounded-full ring-2 
+            top-[-10%] left-1/2 transform -translate-x-1/2 -tranlsate-y-1/2"/>}
         </li>)}
        
       </ul>
       <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
-        <li className="ring-1 ring-redPrim text-redPrim rounded-md py-1 px-4 text-sm cursor-pointer">
-          Small
-        </li>
-        <li className="ring-1 ring-redPrim text-white bg-redPrim rounded-md py-1 px-4 text-sm cursor-pointer">
-          Meduim
-        </li>
-        <li className="ring-1 ring-redPrim text-redPrim rounded-md py-1 px-4 text-sm cursor-pointer">
-          Large
-        </li>
+        {
+          sizes.map((s, i) =>
+            <li 
+              onClick={() => {
+                setIndex(ind => ({...ind, sizeIndex: i}))
+              }}
+                className={ i === index.sizeIndex ?
+                  "ring-1 ring-redPrim text-redPrim rounded-md py-1 px-4 text-sm cursor-pointer" :
+                  "ring-1 ring-white text-white bg-redPrim rounded-md py-1 px-4 text-sm cursor-pointer" 
+                }
+            >
+              {s}
+            </li> 
+        )}
+       
       </ul>
     </div>
   )
