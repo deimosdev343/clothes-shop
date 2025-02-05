8498ad01d6a0430dad21e2b2a00930903f346b1a
eliguy343 <eliguy343@gmail.com>
2025-02-05 21:14:04 +0200

    Bill comp

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,12 +1,17 @@
+import BillComponent from "@/components/checkout/BillComponent"
 import CheckoutComponent from "@/components/checkout/CheckoutComponent"
 
 const CheckoutPage = () => {
 
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col md:flex-row  w-full h-screen overflow-scroll">
       <div className="w-full md:w-[65%] min-h-full flex flex-col items-center">
         <h2 className="font-bold text-4xl mb-2">Items For Checkout</h2>
         <CheckoutComponent/>
+      </div>
+      <div className="w-full md:w-[45%] min-h-full flex flex-col items-center">
+        <h2 className="font-bold text-4xl mb-2">Total Bill</h2>
+        <BillComponent/>
       </div>
     </div>
   )
---
+++ src/components/checkout/BillComponent.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+const BillComponent = () => {
+  
+  return (
+    <div>BillComponent</div>
+  )
+}
+
+export default BillComponent
