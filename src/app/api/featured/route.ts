9ba0dd3cf8988db348e19ed3d0f98b62e3a75e0a
eliguy343 <eliguy343@gmail.com>
2025-10-26 19:28:59 +0200

    get featured

---
+++ src/app/api/featured/route.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { 
+  NextRequest,
+  NextResponse 
+} from "next/server";
+
+export const GET =  async (req: NextRequest) => {
+  try {
+    const category = req.nextUrl.searchParams.get("category");
+    const res = await axios.get(
+      `${process.env.BACKEND_API}/products`,
+      {
+        params:{
+          category,
+        }
+      }
+    );
+    return NextResponse.json(
+      res.data, 
+      {status:200}
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      {msg:"Internal Server Error"}, 
+      {status:500}
+    );
+  }
+}
